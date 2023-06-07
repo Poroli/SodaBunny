279e758759d9010e619489a8c30f146738ed1a1d
Natsume <natsumeniichan@gmail.com>
2023-06-07 15:03:03 +0200

    Debugging debugged

--- Assets/Scripts/DeBuggingPosition.cs
+++ Assets/Scripts/DeBuggingPosition.cs
@@ -16,7 +16,7 @@ public class DeBuggingPosition : MonoBehaviour
         if (other.gameObject.tag == "DeBug")
         {
             rb.velocity = Vector3.zero;
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 2, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(Random.Range(-4.5f, 4.5f), 1.5f, Random.Range(-4.5f, 4.5f));
         }
     }
 }
